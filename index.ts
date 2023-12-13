1b2efeb1ff80509905e223e6aa48194f5cc85a62
Eltik <76538547+Eltik@users.noreply.github.com>
2023-12-12 17:05:51 -0800

    build: fake img lol

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import { RepoMetadata } from "@mochiapp/js";
 let metadata: RepoMetadata = {
     name: "Anify",
     author: "Eltik",
-    description: "Anify module coded by Eltik for https://anify.tv."
+    description: "Anify module coded by Eltik for https://anify.tv.",
+    icon: "https://media.discordapp.net/attachments/1183076192996315156/1184298659291070525/image0.jpg?ex=658b7713&is=65790213&hm=34100a7f2829a901ecb429c9d6c61b255e0ef0a9876199a938b1613eade76205&=&format=webp&width=566&height=1226"
 }
 
 export default metadata;
