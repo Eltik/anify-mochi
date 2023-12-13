c7cd9b85e1dacb77116421ac3a501e8474810d3b
Eltik <76538547+Eltik@users.noreply.github.com>
2023-12-12 16:47:45 -0800

    logs

--- src/module/utils/getServerUrl.ts
+++ src/module/utils/getServerUrl.ts
@@ -1,6 +1,7 @@
 import { Source } from "../models";
 
 export async function getServerSources(url: string): Promise<Source> {
+    console.log("Fetching from " + url + "...")
     const data: Source = await (await request.get(url)).json();
     return data;
 }
