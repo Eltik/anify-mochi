02364a41c1f93f435a331c11677bab68c21fddfa
Eltik <76538547+Eltik@users.noreply.github.com>
2023-12-12 16:59:49 -0800

    remove: icon

--- index.ts
+++ index.ts
@@ -3,8 +3,7 @@ import { RepoMetadata } from "@mochiapp/js";
 let metadata: RepoMetadata = {
     name: "Anify",
     author: "Eltik",
-    description: "Anify module coded by Eltik for https://anify.tv.",
-    icon: "https://anify.tv/_next/image?url=%2Fcharacter.png&w=1080&q=75"
+    description: "Anify module coded by Eltik for https://anify.tv."
 }
 
 export default metadata;
