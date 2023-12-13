6e76671f597afa2408fb4add73e6662e82dc3995
Eltik <76538547+Eltik@users.noreply.github.com>
2023-12-12 17:13:07 -0800

    fix: Manifest.json

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ let metadata: RepoMetadata = {
     name: "Anify",
     author: "Eltik",
     description: "Anify module coded by Eltik for https://anify.tv.",
-    icon: "https://media.discordapp.net/attachments/1183076192996315156/1184298659291070525/image0.jpg?ex=658b7713&is=65790213&hm=34100a7f2829a901ecb429c9d6c61b255e0ef0a9876199a938b1613eade76205&=&format=webp&width=566&height=1226"
+    icon: "https://github.com/Eltik/anify-mochi-module/blob/main/README/character.png"
 }
 
 export default metadata;
